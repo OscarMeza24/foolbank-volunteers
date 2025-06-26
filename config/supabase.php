ac6a986395ddf8cba6a2f8ddd3a38c347f11188a
Oscar Meza <weynomames53@gmail.com>
2025-06-26 05:34:54 -0500

    .

--- config/supabase.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'url' => env('SUPABASE_URL'),
-    'key' => env('SUPABASE_KEY'),
-    'verify_ssl' => env('APP_ENV') === 'local' ? false : true,
-];
--- routes/simple.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestSupabaseController;
-
-Route::post('/insert-data', [TestSupabaseController::class, 'simpleInsert'])->name('simple.insert');
